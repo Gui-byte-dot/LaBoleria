7b4aa3d529461f932c06225009167a97b23d0ebe
Gui-byte-dot <grgomes82@gmail.com>
2023-01-06 09:59:39 -0300

    initial

---
+++ src/models/Cakes.js
@@ -0,0 +1,9 @@
+import joi from 'joi';
+export const cakeSchema = joi.object({
+    name:joi.string().min(2).required(),
+    price:joi.number().min(0).required(),
+    description:joi.string().required(),
+    
+
+    
+})
