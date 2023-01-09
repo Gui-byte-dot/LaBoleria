9759aca46c92f79d40bbb446178d93ee4075d3c7
Gui-byte-dot <grgomes82@gmail.com>
2023-01-09 00:52:47 -0300

    projeto

--- src/controllers/orders.controllers.js
+++ src/controllers/orders.controllers.js
@@ -10,65 +10,131 @@ export async function create(req,res){
     }
 }
 export async function findAllOrders(req,res){
-    const date = req.query;
-    try{
-        const getAllOrders = await connectionDB.query(`
-        SELECT 
-            clients.id AS "clientId",
-            clients.name AS "clientName",
-            clients.address AS "clientAddress",
-            clients.phone AS "clientPhone",
-            cakes.id AS "cakeId",
-            cakes.name AS "cakeName",
-            cakes.price AS "cakePrice",
-            cakes.description AS "cakeDescription",
-            cakes.image AS "cakeImage",
-            orders.id AS "orderId",
-            orders."createdAt" AS "createdAt",
-            orders.quantity AS "quantity",
-            orders."totalPrice" AS "totalPrice"
-        FROM 
-            orders
-        JOIN 
-            clients 
-        ON
-            orders."clientId" = clients.id
-        JOIN
-            cakes
-        ON 
-            orders."cakeId" = cakes.id
-       
-        `)
-        const allOrders = getAllOrders.rows.map((ord) => ({
-            client:{
-                id:ord.clientId,
-                name: ord.clientName,
-                address:ord.clientAddress,
-                phone:ord.clientPhone
-            },
-            cake: {
-                id:ord.cakeId,
-                name:ord.cakeName,
-                price:ord.cakePrice,
-                description:ord.cakeDescription,
-                image:ord.cakeImage
-            },
-            orderId:ord.orderId,
-            createdAt:ord.createdAt,
-            quantity:ord.quantity,
-            totalPrice:ord.totalPrice
-        }))
-            
-        if(getAllOrders.rowCount === 0){
-            return res.send([]).status(404);
-        }
+    const { date } = req.query;
 
-        res.send(allOrders).status(200);
-        console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'));
-    }catch(err){
-        return res.sendStatus(500)
-    }
+    if(date){
+        try{
+        
+            const getAllOrders = await connectionDB.query(`
+            SELECT 
+                clients.id AS "clientId",
+                clients.name AS "clientName",
+                clients.address AS "clientAddress",
+                clients.phone AS "clientPhone",
+                cakes.id AS "cakeId",
+                cakes.name AS "cakeName",
+                cakes.price AS "cakePrice",
+                cakes.description AS "cakeDescription",
+                cakes.image AS "cakeImage",
+                orders.id AS "orderId",
+                TO_CHAR(orders."createdAt",'YYYY-MM-DD HH24:MI:SS') AS "createdAt",
+                orders.quantity AS "quantity",
+                orders."totalPrice" AS "totalPrice"
+            FROM 
+                orders
+            JOIN 
+                clients 
+            ON
+                orders."clientId" = clients.id
+            JOIN
+                cakes
+            ON 
+                orders."cakeId" = cakes.id
+            WHERE 
+                orders."createdAt"::date = $1
     
+    
+            `,[`${date}`]);
+            const allOrders = getAllOrders.rows.map((ord) => ({
+                client:{
+                    id:ord.clientId,
+                    name: ord.clientName,
+                    address:ord.clientAddress,
+                    phone:ord.clientPhone
+                },
+                cake: {
+                    id:ord.cakeId,
+                    name:ord.cakeName,
+                    price:ord.cakePrice,
+                    description:ord.cakeDescription,
+                    image:ord.cakeImage
+                },
+                orderId:ord.orderId,
+                createdAt:ord.createdAt,
+                quantity:ord.quantity,
+                totalPrice:ord.totalPrice
+            }))
+                
+            if(getAllOrders.rowCount === 0){
+                return res.send([]).status(404);
+            }
+            
+            res.send(allOrders).status(200);
+        }catch(err){
+            return res.sendStatus(500)
+        }
+    } else {
+        try{
+        
+            const getAllOrders = await connectionDB.query(`
+            SELECT 
+                clients.id AS "clientId",
+                clients.name AS "clientName",
+                clients.address AS "clientAddress",
+                clients.phone AS "clientPhone",
+                cakes.id AS "cakeId",
+                cakes.name AS "cakeName",
+                cakes.price AS "cakePrice",
+                cakes.description AS "cakeDescription",
+                cakes.image AS "cakeImage",
+                orders.id AS "orderId",
+                TO_CHAR(orders."createdAt",'YYYY-MM-DD HH24:MI:SS') AS "createdAt",
+                orders.quantity AS "quantity",
+                orders."totalPrice" AS "totalPrice"
+            FROM 
+                orders
+            JOIN 
+                clients 
+            ON
+                orders."clientId" = clients.id
+            JOIN
+                cakes
+            ON 
+                orders."cakeId" = cakes.id
+           
+    
+    
+            `);
+            const allOrders = getAllOrders.rows.map((ord) => ({
+                client:{
+                    id:ord.clientId,
+                    name: ord.clientName,
+                    address:ord.clientAddress,
+                    phone:ord.clientPhone
+                },
+                cake: {
+                    id:ord.cakeId,
+                    name:ord.cakeName,
+                    price:ord.cakePrice,
+                    description:ord.cakeDescription,
+                    image:ord.cakeImage
+                },
+                orderId:ord.orderId,
+                createdAt:ord.createdAt,
+                quantity:ord.quantity,
+                totalPrice:ord.totalPrice
+            }))
+                
+            if(getAllOrders.rowCount === 0){
+                return res.send([]).status(404);
+            }
+            
+            res.send(allOrders).status(200);
+            console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'));
+        }catch(err){
+            return res.sendStatus(500)
+        }
+    }
 }
 
 export async function findOrderId(req,res){
@@ -86,7 +152,7 @@ export async function findOrderId(req,res){
             cakes.description AS "cakeDescription",
             cakes.image AS "cakeImage",
             orders.id AS "orderId",
-            orders."createdAt" AS "createdAt",
+            TO_CHAR(orders."createdAt",'YYYY-MM-DD HH24:MI:SS') AS "createdAt",
             orders.quantity AS "quantity",
             orders."totalPrice" AS "totalPrice"
         FROM 
@@ -117,7 +183,7 @@ export async function findOrderId(req,res){
                 image:ord.cakeImage
             },
             orderId:ord.orderId,
-            createdAt:ord.createdAt,
+            createdAt:ord.createdAt.replace('.000Z',''),
             quantity:ord.quantity,
             totalPrice:ord.totalPrice
         }))
--- tables.sql
+++ tables.sql
@@ -1,30 +1,23 @@
 CREATE TABLE cakes (
-   “id” serial NOT NULL PRIMARY KEY,
-   “name” varchar NOT NULL UNIQUE,
-    “price” numeric NOT NULL,
-    “image” varchar NOT NULL,
-    “description” TEXT NOT NULL
-
+        id SERIAL PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        price NUMERIC NOT NULL,
+        image VARCHAR NOT NULL,
+        description TEXT NOT NULL
 );
+
 CREATE TABLE clients (
-   “id” serial NOT NULL  PRIMARY KEY,
-   “name” varchar NOT NULL,
-   “address” varchar NOT NULL, 
-   “phone” varchar NOT NULL
+        id SERIAL PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        address VARCHAR NOT NULL,
+        phone VARCHAR NOT NULL
 );
+
 CREATE TABLE orders (
-	"id" SERIAL PRIMARY KEY,
-	"clientId" INTEGER NOT NULL,
- 	"cakeId" INTEGER NOT NULL,
-	"quantity" INTEGER NOT NULL,
-	"createdAt" TIMESTAMP NOT NULL,
-	"totalPrice" NUMERIC NOT NULL, 
-
-	CONSTRAINT "FK_CLIENT" FOREIGN KEY ("clientId")
-		REFERENCES clients("id")
-		ON DELETE CASCADE,
-	CONSTRAINT "FK_CAKE" FOREIGN KEY ("cakeId")
-		REFERENCES cakes("id")
-		ON DELETE CASCADE
-
+        id SERIAL PRIMARY KEY,
+        "clientId" INTEGER NOT NULL,
+        "cakeId" INTEGER NOT NULL,
+        quantity INTEGER NOT NULL,
+    "createdAt" TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
+    "totalPrice" NUMERIC NOT NULL
 );
